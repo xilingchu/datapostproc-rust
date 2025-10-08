18f0660befae361125b24c37909c89f49972d65d
xilingchu <xilingchu@163.com>
2025-10-09 06:01:04 +0800

    Add impl for data and h5file.

--- src/data.rs
+++ src/data.rs
@@ -6,8 +6,8 @@ use crate::hdf5::Block;
 pub struct H5File 
 {
     filename: String,
-    var: Vec<String>,
-    data: HashMap<String, Data>,
+    variables: Vec<String>,
+    datasets: HashMap<String, Data>,
 }
 
 pub struct Data
@@ -15,4 +15,25 @@ pub struct Data
     name: String,
     block: Block,
     dataset: Dataset,
+}
+
+impl H5File {
+    fn new(self, filename: String) -> Self {
+        Self {
+            filename,
+            variables: Vec::new(),
+            datasets: HashMap::new(),
+        }
+    }
+
+    fn add_dataset(&mut self, name: &str, data:Data) {
+        self.variables.push(String::from(name));
+        self.datasets.insert(String::from(name), data);
+    }
+}
+
+impl Data {
+    pub fn new(name:String, block: Block, dataset: Dataset) -> Self {
+        Self {name, block, dataset}
+    }
 }
