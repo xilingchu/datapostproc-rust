1c276486424896293d789a6656c40c552c516edc
xilingchu <xilingchu@163.com>
2025-10-09 13:22:17 +0800

    Change T to f64 in centerline.rs

--- src/math/centerline.rs
+++ src/math/centerline.rs
@@ -1,21 +1,19 @@
 /// Control may change the position of the center line.
 /// Centerline was defined through dudz
 
-use ndarray::Array1;
+use ndarray::{Array1};
 
-pub fn get_centerline<T>(u_slice: &Array1<T>, uz_slice: &Array1<T>, z: &Array1<T>) -> T 
-    where T: Zero + Copy
+pub fn get_centerline<T>(u_slice: &Array1<f64>, uz_slice: &Array1<f64>, z: &Array1<f64>) -> f64 
         {
         let len = uz_slice.len();
         let mut locate = 0usize;
         for i in 0..(len-1) {
-            if uz_slice[i] * uz_slice[i+1] < T::Zero() {
+            if uz_slice[i] * uz_slice[i+1] < 0f64 {
                 locate = i;
                 break;
             }
         }
     // Locate the location where split uz>0 and uz<0.
-    let k = (&u_slice[locate+1] - &u_slice[locate])/(&z[locate+1] - &z[locate]);
-    let result = (&z[locate+1]-&z[locate])*2*&u_slice[locate]/(&u_slice[locate+1] - &u_slice[locate]) + &z[locate];
+    let result = (z[locate+1]-z[locate])*2f64*u_slice[locate]/(u_slice[locate+1] - u_slice[locate]) + z[locate];
     return result
 }
