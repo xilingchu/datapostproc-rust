5d53953a5cb40eb4a71fe72184dbb9493e04822f
xilingchu <xilingchu@163.com>
2026-04-09 09:55:41 +0800

    Add function to read DNS info and coords.

--- datapostproc/src/data.rs
+++ datapostproc/src/data.rs
@@ -3,19 +3,16 @@ use std::collections::HashMap;
 use hdf5::{Dataset, File, Error, H5Type};
 use crate::hdf5::{Block, H5Data, DatasetHyperslabExt};
 
-
-pub struct H5File
-{
+pub struct H5File {
     file: File,
     filename: String,
     info: DNSInfo,
     variables: Vec<String>,
-    coords : HashMap<String, Data>,
+    coords: HashMap<String, Data>,
     datasets: HashMap<String, Data>,
 }
 
-pub struct Data
-{
+pub struct Data {
     name: String,
     block: Option<Block>,
     dataset: Dataset,
@@ -23,21 +20,36 @@ pub struct Data
 
 #[derive(Debug, Default)]
 pub struct DNSInfo {
-    nx: Option<i32>,
-    ny: Option<i32>,
-    nz: Option<i32>,
-    lx: Option<f64>,
-    ly: Option<f64>,
-    lz: Option<f64>,
-    re: Option<f64>,
-    dt: Option<f64>,
-    is_periodic: bool,
-    is_defined: bool
+    pub nx: Option<i32>,
+    pub ny: Option<i32>,
+    pub nz: Option<i32>,
+    pub re: Option<f64>,
+    pub nu: Option<f64>,
+    pub is_periodic: bool,
+    pub is_defined: bool,
+}
+
+impl DNSInfo {
+    pub fn from_h5file(file: &hdf5::File) -> hdf5::Result<Self> {
+        let nx = file.dataset("x")?.shape()[0] as i32;
+        let ny = file.dataset("y")?.shape()[0] as i32;
+        let nz = file.dataset("zc")?.shape()[0] as i32;
+        let nu = file.dataset("nu")?.read_scalar::<f64>()?;
+        Ok(Self {
+            nx: Some(nx),
+            ny: Some(ny),
+            nz: Some(nz),
+            nu: Some(nu),
+            re: Some(1.0 / nu),
+            is_periodic: false,
+            is_defined: true,
+        })
+    }
 }
 
 impl H5File {
-    fn new(filename: &str) -> Result<Self, Error> {
-        let file = File::open(&filename)?;
+    pub fn new(filename: &str) -> Result<Self, Error> {
+        let file = File::open(filename)?;
         Ok(Self {
             file,
             filename: String::from(filename),
@@ -48,52 +60,67 @@ impl H5File {
         })
     }
 
-        let dataset = self.file.dataset(name)?;
-        let data = Data::new(String::from(name), block, dataset);
-        self.coords.insert(String::from(name), data);
-        Ok(())
-    }
-
-    fn add_dataset(&mut self, name: &str, block:Option<Block>) -> Result<(), Error> {
+    pub fn add_dataset(&mut self, name: &str, block: Option<Block>) -> Result<(), Error> {
         self.variables.push(String::from(name));
         let dataset = self.file.dataset(name)?;
         let data = Data::new(String::from(name), block, dataset);
         self.datasets.insert(String::from(name), data);
         Ok(())
     }
-    
-    fn get_info(&mut self) -> Result<(), Error> {
-        info_iter = 
+
+    pub fn get_info(&mut self) -> Result<(), Error> {
+        self.info = DNSInfo::from_h5file(&self.file)?;
+        Ok(())
+    }
+
+    pub fn load_coords(&mut self) -> Result<(), Error> {
+        let names = self.file.member_names()?;
+        for name in names {
+            if name.starts_with('x') || name.starts_with('y') || name.starts_with('z') {
+                let dataset = self.file.dataset(&name)?;
+                let data = Data::new(name.clone(), None, dataset);
+                self.coords.insert(name, data);
+            }
+        }
+        Ok(())
+    }
+
+    pub fn coord(&self, name: &str) -> Option<&Data> {
+        self.coords.get(name)
+    }
+
+    pub fn info(&self) -> &DNSInfo {
+        &self.info
+    }
+
+    pub fn info_mut(&mut self) -> &mut DNSInfo {
+        &mut self.info
     }
 }
 
-impl Data{
-    pub fn new(name:String, block: Option<Block>, dataset: Dataset) -> Self {
-        Self {name, block, dataset}
+impl Data {
+    pub fn new(name: String, block: Option<Block>, dataset: Dataset) -> Self {
+        Self { name, block, dataset }
     }
 
-    // Read data through chunking
-    fn read_data<T>(
-            &self,
-        ) -> Result<H5Data<T>, Error>
-        where
-            T: H5Type + Copy
+    pub fn read_data<T>(&self) -> Result<H5Data<T>, Error>
+    where
+        T: H5Type + Copy,
     {
         match &self.block {
             None => {
                 if self.dataset.is_single() {
-                    self.dataset.read_1d::<T>()?.first().copied().map(H5Data::Scalar).ok_or_else(|| Error::from("Empty dataset"))
+                    self.dataset
+                        .read_1d::<T>()?
+                        .first()
+                        .copied()
+                        .map(H5Data::Scalar)
+                        .ok_or_else(|| Error::from("Empty dataset"))
                 } else {
                     self.dataset.read_dyn().map(H5Data::Array)
                 }
             }
-            Some(block) => {
-                if self.dataset.is_single() {
-                    self.dataset.read_1d::<T>()?.first().copied().map(H5Data::Scalar).ok_or_else(|| Error::from("Empty dataset"))
-                } else {
-                    self.dataset.read_hyperslab(block.clone()).map(H5Data::Array)
-                }
-            }
+            Some(block) => self.dataset.read_hyperslab(block.clone()).map(H5Data::Array),
         }
     }
 }
